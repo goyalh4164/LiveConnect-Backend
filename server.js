be4cc0a4bab6af2553327136c1b21118f203d5d5
Harsh <goyalh4164@gmail.com>
2024-02-02 20:29:46 +0530

    Stable Version One

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ import http from 'http';
 import { Server } from 'socket.io';
 import app from './index.js';
 import { connectToDb } from './src/config/db.js';
+
 const PORT = 8000;
 
 const server = http.createServer(app);
-const io = new Server(server,{
+const io = new Server(server, {
   cors: {
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST'],
@@ -13,20 +14,35 @@ const io = new Server(server,{
   },
 });
 
+// Map to store roomID for each user
+const userRooms = new Map();
+
 // Socket.io connection event
 io.on('connection', (socket) => {
   console.log('A user connected');
 
+  // Handle joining a room
+  socket.on('join-room', ({ roomID }) => {
+    console.log(`User joined room: ${roomID}`);
+    socket.join(roomID);
+    userRooms.set(socket.id, roomID); // Map socket.id to roomID
+  });
+
   // Handle messages
   socket.on('message', (data) => {
     console.log('Message:', data);
-    // Broadcast the message to all connected clients
-    io.emit('message', data);
+    // Broadcast the message to all connected clients in the room
+    io.to(userRooms.get(socket.id)).emit('message', data);
   });
 
   // Disconnect event
   socket.on('disconnect', () => {
     console.log('A user disconnected');
+    const roomID = userRooms.get(socket.id);
+    if (roomID) {
+      socket.leave(roomID);
+      userRooms.delete(socket.id);
+    }
   });
 });
 
