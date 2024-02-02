43d62b45446f92df15fb3b46db3e2512c2c3980b
Harsh <goyalh4164@gmail.com>
2024-02-02 16:10:26 +0530

    added socket.io

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
-
-import app from "./index.js";
+import http from 'http';
+import { Server } from 'socket.io';
+import app from './index.js';
 import { connectToDb } from './src/config/db.js';
 
 const PORT = 8000;
 
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.listen(PORT, async () => {
+// Socket.io connection event
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Handle messages
+  socket.on('message', (data) => {
+    console.log('Message:', data);
+    // Broadcast the message to all connected clients
+    io.emit('message', data);
+  });
+
+  // Disconnect event
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(PORT, async () => {
   await connectToDb();
   console.log(`Server is live at PORT ${PORT}`);
 });
